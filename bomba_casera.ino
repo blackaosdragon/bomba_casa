6a83312082128c7c235574a331bf20ce972c902c
blackaosdragon <ka_ivj@live.com>
2025-05-18 08:16:04 -0600

    Se agrego todas los ultimos cambios de la bomba

--- bomba_casera.ino
+++ bomba_casera.ino
@@ -13,6 +13,10 @@ const int sensorMaximoRecipiente2 = 51;
 const int sensorMinimoRecipiente1 = 52;
 const int sensorMaximoRecipiente1 = 53;
 
+const int sensorPrincipalBajo = 8;
+const int sensorPrincipalMedio = 9;
+const int sensorPrincipalLleno = 10;
+
 // Pines de la bomba y el LED
 const int bombaAgua = 2;
 const int ledError = 41;
@@ -25,6 +29,7 @@ const int recipiente5 = 3;
 // const int recipiente6 = 2;
 
 int bombaActiva = 0;
+int numeroCiclo = 0;
 bool valvulaRecipiente1Activa = 0;
 bool valvulaRecipiente2Activa = 0;
 bool valvulaRecipiente3Activa = 0;
@@ -42,6 +47,10 @@ void setup()
     pinMode(sensorMaximoRecipiente2, INPUT);
     pinMode(sensorMinimoRecipiente3, INPUT);
     pinMode(sensorMaximoRecipiente3, INPUT);
+    pinMode(sensorPrincipalBajo, INPUT);
+    pinMode(sensorPrincipalMedio, INPUT);
+    pinMode(sensorPrincipalLleno, INPUT);    
+    
     pinMode(bombaAgua, OUTPUT);
     pinMode(ledError, OUTPUT);
     pinMode(recipiente1, OUTPUT);
@@ -64,6 +73,7 @@ void setup()
 
     // Inicializa la comunicación serie
     Serial.begin(9600);
+    bombaActiva = 0;
 }
 
 void loop()
@@ -79,68 +89,115 @@ void loop()
     bool nivelMaximoRecipiente4 = digitalRead(sensorMaximoRecipiente4);
     bool nivelMinimoRecipiente5 = digitalRead(sensorMinimoRecipiente5);
     bool nivelMaximoRecipiente5 = digitalRead(sensorMaximoRecipiente5);
+    bool nivelSensorPrincipalVacio = digitalRead(sensorPrincipalBajo);
+    bool nivelSensorPrincipalMedio = digitalRead(sensorPrincipalMedio);
+    bool nivelSensorPrincipalLleno = digitalRead(sensorPrincipalLleno);    
     
-
-    if(nivelMinimoRecipiente1 == false && nivelMaximoRecipiente1 == false){
+    if(nivelMinimoRecipiente1 == false){
       arregloDeValvulas[0] = 1;
-      bombaActiva = 1;
-      valvulaRecipiente1Activa = 1;
-      valvulaRecipiente2Activa = 0;
-      valvulaRecipiente3Activa = 0;
-      valvulaRecipiente4Activa = 0;
-      valvulaRecipiente5Activa = 0;
     }
-    if(nivelMinimoRecipiente2 == false && nivelMaximoRecipiente2 == false){
+    if(nivelMaximoRecipiente1 == true){
+      arregloDeValvulas[0] = 0;
+    }
+    if(nivelMinimoRecipiente2 == false){
       arregloDeValvulas[1] = 1;
-      bombaActiva = 1;
-      valvulaRecipiente1Activa = 0;
-      valvulaRecipiente2Activa = 1;
-      valvulaRecipiente3Activa = 0;
-      valvulaRecipiente4Activa = 0;
-      valvulaRecipiente5Activa = 0;
     }
-    if(nivelMinimoRecipiente3 == false && nivelMaximoRecipiente3 == false){
+    if(nivelMaximoRecipiente2 == true){
+      arregloDeValvulas[1] = 0;
+    }
+    if(nivelMinimoRecipiente3 == false){
       arregloDeValvulas[2] = 1;
-      bombaActiva = 1;
-      valvulaRecipiente1Activa = 0;
-      valvulaRecipiente2Activa = 0;
-      valvulaRecipiente3Activa = 1;
-      valvulaRecipiente4Activa = 0;
-      valvulaRecipiente5Activa = 0;
     }
-    if(nivelMinimoRecipiente4 == false && nivelMaximoRecipiente4 == false){
+    if(nivelMaximoRecipiente3 == true){
+      arregloDeValvulas[2] = 0;
+    }
+    if(nivelMinimoRecipiente4 == false){
       arregloDeValvulas[3] = 1;
-      bombaActiva = 1;
-      valvulaRecipiente1Activa = 0;
-      valvulaRecipiente2Activa = 0;
-      valvulaRecipiente3Activa = 0;
-      valvulaRecipiente4Activa = 1;
-      valvulaRecipiente5Activa = 0;
     }
-    if(nivelMinimoRecipiente5 == false && nivelMaximoRecipiente5 == false){
+    if(nivelMaximoRecipiente4 == true){
       arregloDeValvulas[3] = 0;
-      arregloDeValvulas[4] = 1;
-      bombaActiva = 1;
-      valvulaRecipiente1Activa = 0;
-      valvulaRecipiente2Activa = 0;
-      valvulaRecipiente3Activa = 0;
-      valvulaRecipiente4Activa = 0;
-      valvulaRecipiente5Activa = 1;
     }
-    if(
-      nivelMinimoRecipiente5 == true && nivelMaximoRecipiente5 == true &&
-      nivelMinimoRecipiente4 == true && nivelMaximoRecipiente4 == true &&
-      nivelMinimoRecipiente3 == true && nivelMaximoRecipiente3 == true &&
-      nivelMinimoRecipiente2 == true && nivelMaximoRecipiente2 == true &&
-      nivelMinimoRecipiente1 == true && nivelMaximoRecipiente1 == true 
-      ){
-        bombaActiva = 0;
-        valvulaRecipiente1Activa = 0;
-      valvulaRecipiente2Activa = 0;
-      valvulaRecipiente3Activa = 0;
-      valvulaRecipiente4Activa = 0;
-      valvulaRecipiente5Activa = 0;
+    if(nivelMinimoRecipiente5 == false){
+      arregloDeValvulas[4] = 1;
+    }
+    if(nivelMaximoRecipiente5 == true){
+      arregloDeValvulas[4] = 0;
+    }
+    Serial.print("Tanque 1: Min: ");
+    Serial.print(nivelMinimoRecipiente1);
+    Serial.print("Max: ");
+    Serial.print(nivelMaximoRecipiente1);
+    Serial.print(" / ");
+    
+      if(numeroCiclo == 0){
+        if(nivelMinimoRecipiente1==false){
+          valvulaRecipiente1Activa = true;
+        }
+        if(nivelMaximoRecipiente1 == true){
+          valvulaRecipiente1Activa = false;
+          numeroCiclo = numeroCiclo + 1;
+        }
       }
+      if(numeroCiclo == 1){
+        if(nivelMinimoRecipiente2==false){
+          valvulaRecipiente2Activa = true;
+        }
+        if(nivelMaximoRecipiente2 == true){
+          valvulaRecipiente2Activa = false;
+          numeroCiclo = numeroCiclo+1;
+        }
+      }
+      if(numeroCiclo == 2){
+        if(nivelMinimoRecipiente3==false){
+          valvulaRecipiente3Activa = true;          
+        }
+        if(nivelMaximoRecipiente3 == true){
+          valvulaRecipiente3Activa = false;
+          numeroCiclo = numeroCiclo+1;
+        }
+      }
+      if(numeroCiclo == 3){
+        if(nivelMinimoRecipiente4==false){
+          valvulaRecipiente4Activa = true;
+        }
+        if(nivelMaximoRecipiente4 == true){
+          valvulaRecipiente4Activa = false;
+          numeroCiclo = numeroCiclo+1;          
+        }
+      }
+      if(numeroCiclo == 4){
+        if(nivelMinimoRecipiente5==false){
+          valvulaRecipiente5Activa = true;
+        }
+        if(nivelMaximoRecipiente5 == true){
+          valvulaRecipiente5Activa = false;
+          numeroCiclo = numeroCiclo+1;          
+        }
+      }
+
+      
+    if(nivelSensorPrincipalVacio == true && nivelSensorPrincipalMedio == true && nivelSensorPrincipalLleno == true){
+      bombaActiva = 1;
+    }
+    if(nivelSensorPrincipalVacio == false){
+      bombaActiva = 0; 
+    }
+    if(valvulaRecipiente5Activa==false && valvulaRecipiente4Activa==false && valvulaRecipiente3Activa==false && valvulaRecipiente2Activa==false && valvulaRecipiente1Activa==false){
+      bombaActiva = 0;
+    }
+    // Serial.print("arregloDeValvulas:" );
+    // Serial.print(arregloDeValvulas[numeroCiclo]);
+    Serial.print(" ");
+    Serial.print("numeroCiclo: ");
+    Serial.print(numeroCiclo);
+    Serial.print(" ");
+    // numeroCiclo = numeroCiclo+1;
+    
+    
+    if(numeroCiclo >= 5){
+      numeroCiclo = 0;
+    }
+    //arregloDeValvulas[5] = {0, 0, 0, 0, 0};
     
     digitalWrite(bombaAgua, bombaActiva);
     digitalWrite(recipiente1, valvulaRecipiente1Activa);
@@ -154,15 +211,18 @@ void loop()
     // Serial.println(nivelMaximoRecipiente1);
     Serial.print("Bomba:" );
     Serial.println(bombaActiva);    
-    Serial.print("Arreglo de bombas:" );
+    // Serial.print("Arreglo de bombas: ");
     Serial.print(arregloDeValvulas[0]);   
     Serial.print(arregloDeValvulas[1]); 
     Serial.print(arregloDeValvulas[2]);   
     Serial.print(arregloDeValvulas[3]);   
-    Serial.print(arregloDeValvulas[4]);   
+    Serial.print(arregloDeValvulas[4]); 
+    Serial.print(" "); 
+    
+    
 
 
     //", nivelMinimoRecipiente1 ," max: ",nivelMaximoRecipiente1
     // Espera 500 ms antes de la siguiente lectura
-    delay(500);
+    delay(1000);
 }
